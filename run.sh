4949dd1b17675f952476fd169e4ff016c21f9529
ch4og <mitanick@ya.ru>
2024-10-29 17:20:12 +0300

    feat: control auth with env vars

--- run.sh
+++ run.sh
@@ -6,4 +6,18 @@ cd nodecg || {
 	exit 1
 }
 
+# Requre to login to nodecg
+export NODECG_LOGIN=0
+
+# String that is used to encrypt sessions
+# CHANGE THIS IF YOU ARE USING NODECG_LOGIN
+export NODECG_SECRET="CSM is the best"
+
+# Discord bot information
+# It will allow all members of the server selected to use the bot
+# SET THIS IF YOU ARE USING NODECG_LOGIN
+export NODECG_DISCORD_CLIENT=""
+export NODECG_DISCORD_SECRET=""
+export NODECG_DISCORD_SERVER_ID=""
+
 node .
